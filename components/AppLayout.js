46cf143685e514bcdc6a00ee52b22b6efea16bfb
Taewoong1378 <ktw2378@korea.ac.kr>
2021-07-22 21:35:06 +0900

    7/22 21:35 desktop commit

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
+import styled from 'styled-components';
+
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+
+const SearchInput = styled(Input.Search)`
+    vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div>
             <div>
@@ -15,26 +24,21 @@ const AppLayout = ({ children }) => {
                         <Link href="/profile"><a>프로필</a></Link>
                     </Menu.Item>
                     <Menu.Item>
-                        <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+                        <SearchInput enterButton />
                     </Menu.Item>
                     <Menu.Item>
                         <Link href="/signup"><a>회원가입</a></Link>
                     </Menu.Item>
                 </Menu>
-                {/* 모바일에서는 xs, 테블릿에서는 md
-                총 24개의 컬럼으로 구성돼있는데 xs={24}를 하면 24개의 칼럼, 즉 100%를 다 차지하겠다는 말이다
-                화면이 커지면 6/24 = 25%, 12/24 = 50%, 6/24 = 25%를 각각 차지하겠다는 말이다
-                gutter는 컬럼들이 너무 서로 붙어있으면 쫌 그렇기 때문에 컬럼 사이에 간격을 주는 것이다.                
-                target="blank"만 사용하면 보안위협이 있기 때문에 반드시 rel="noreferrer noopener"를 같이 써준다 */}
                 <Row gutter={8}>
                     <Col xs={24} md={6}>
-                        왼쪽 메뉴
+                        {isLoggedIn} ? <UserProfile /> : <LoginForm />
                     </Col>
                     <Col xs={24} md={12}>
                         {children}
                     </Col>
                     <Col xs={24} md={6}>
-                        오른쪽 메뉴
+                        <a href="https://github.com/Taewoong1378" target="_blank" rel="noreferrer noopener">Made by Taewoong Kang</a>
                     </Col>
                 </Row>
             </div>
