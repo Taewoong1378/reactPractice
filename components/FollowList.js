316e3a06fd6a8ef0ce4bdb6959430cda84e56055
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-07-28 11:22:41 +0900

    인라인 스타일 styled-component & usememo로 빼주기

--- components/FollowList.js
+++ components/FollowList.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { List, Button, Card } from 'antd';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { StopOutlined } from '@ant-design/icons';
 
+const ListItemWrapper = styled(List.Item)`
+    margin-top: 20;
+`;
+
 const FollowList = ({ header, data }) => {
+
+    const listStyled = useMemo(() => ({ marginBottom: 20 }), []);
+    const grid = useMemo(() => ({ gutter: 4, xs: 2, md: 3 }), []);
+    const style = useMemo(() => ({ textAlign: 'center', margin: '10px 0' }), []);
+
     return (
         <List
-            style={{ marginBottom: 20 }}
-            grid={{ gutter: 4, xs: 2, md: 3 }}
+            style={listStyled}
+            grid={grid}
             size="small"
             header={<div>{header}</div>}
-            loadMore={<div style={{ textAlign: 'center', margin: '10px 0'}}><Button>더 보기</Button></div>}
+            loadMore={<div style={style}><Button>더 보기</Button></div>}
             bordered
             dataSource={data}
             renderItem={(item) => (
-                <List.Item style={{ marginTop: 20 }}>
+                <ListItemWrapper>
                     <Card actions={[<StopOutlined key="stop" />]}>
                         <Card.Meta description={item.nickname} />
                     </Card>
-                </List.Item>
+                </ListItemWrapper>
             )}
         />
     );
